4dd8675a14652362c901f1117f7cd452e5c50c90
outfrost <kotlet.bahn@gmail.com>
2020-01-25 11:54:25 +0100

    Fix formatting

--- Japco/Assets/Scripts/ScreenLoader.cs
+++ Japco/Assets/Scripts/ScreenLoader.cs
@@ -1,23 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ScreenLoader : MonoBehaviour
-{
-    public void LoadNextScene()
-    {
-        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
-        SceneManager.LoadScene(currentSceneIndex + 1);
-    }
+public class ScreenLoader : MonoBehaviour {
 
-    public void LoadStartScene()
-    {
-        SceneManager.LoadScene(0);
-    }
-    
-    public void QuitGame()
-    {
-        Application.Quit();
-    }
+	public void LoadNextScene() {
+		int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+		SceneManager.LoadScene(currentSceneIndex + 1);
+	}
+
+	public void LoadStartScene() {
+		SceneManager.LoadScene(0);
+	}
+
+	public void QuitGame() {
+		Application.Quit();
+	}
+
 }
